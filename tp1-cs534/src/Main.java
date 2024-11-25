690eaec21aa32d8ef690a65af5d805e1f46b2b0b
KoZiIS <145428282+KoZiIS@users.noreply.github.com>
2024-11-25 20:36:58 +0100

    Delete tp1-cs534 directory

--- tp1-cs534/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-import madkit.kernel.Madkit;
-
-public class Main {
-
-	public static void main(String[] args) {
-		new Madkit("--launchAgents", CounterAgent.class.getName() + ",true,1;", EmitterAgent.class.getName()+ ",true,1;",ControllerAgent.class.getName()+",true,1;");
-	}
-}
